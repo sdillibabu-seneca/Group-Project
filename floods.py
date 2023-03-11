61fdf3b489c683f524e9ec11b9239ef12649e63f
ShadowRailgun <55095587+ShadowRailgun@users.noreply.github.com>
2023-03-11 11:08:25 -0500

    Added help statements

--- floods.py
+++ floods.py
@@ -1,6 +1,7 @@
 def bind_dos(values):
     print("Launching multiple packets of DNS to DNS port")
     valid_var = ["target_ip", "query_type", "query_name","quantity"]
+    help_statement = "\nsends a multitude of DNS queries to attempt to overwhelm the target"
     bind = []
     variable_input(valid_var, help_statement)
     all_variables_inputted(valid_var)
@@ -16,6 +17,7 @@ def bind_dos(values):
 def syn_flood(values):
     print("SYN Flood")
     valid_var = ["target_ip", "quantity"]
+    help_statement = "\nsends a multitude of SYN packets to attempt to overwhelm the target"
     variable_input(valid_var, help_statement)
     all_variables_inputted(valid_var)
     check_var(values, valid_var)
